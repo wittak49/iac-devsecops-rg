7746c88516494a878522c6c377e614bf54d624df
admin <admin@contoso.com>
2025-10-21 09:12:45 +0000

    edit file

--- main.tf
+++ main.tf
@@ -1,24 +1,25 @@
 resource "azurerm_resource_group" "rg" {
     name = var.resource_group_name
     location = var.location
-}
+} 
 
-resource "azurem_network_security_group" "example_nsg"{
-    name = "iac-dev-nsg"
-    location = azurerm_resource_group.rg.location
+resource "azurerm_network_security_group" "example_nsg" {
+    name                = "iac-devops-nsg"
+    location            = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurem_network_security_rule" "insecure_ssh"{
-    name = "Allow-SSH-Pubilc"
-    priority = 100
-    direction = "Inbound"
-    acess = "Allow"
-    protocol = "Tcp"
-    source_port_rang = "*"
-    description_port_rang ="22"
-    source_address_prefix = "*"
-    description_address_prefix = "*"
-    network_security_group_name = azurem_network_security_group.example_nsg
-    resource_group_name = azurerm_resource_group.rg.name
+
+resource "azurerm_network_security_rule" "insecure_ssh" {
+    name                        = "Allow-SSH-Public"
+    priority                    = 100
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "22"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+    network_security_group_name = azurerm_network_security_group.example_nsg.name
+    resource_group_name         = azurerm_resource_group.rg.name
 }
--- varirable.tf
+++ varirable.tf
@@ -1,11 +1,11 @@
 variable "resource_group_name"{
-    description = "Name of Resource Group"
-    type = string
-    default = "iac-devops-rg-14"
+    description = "Name of  Resource Group"
+    type        =  string
+    default     = "iac-devops-rg-14"
 }
 
 variable "location" {
-  description = "Region of Azure"
-  type = string
-  default = "East Us"
+    description = "Region of Azure"
+    type        = string
+    default     = "East US"
 }
