aeb6383aae23cb6864b48dd6835b4456df2485a6
admin <admin@contoso.com>
2025-10-21 09:03:38 +0000

    edit file

--- varirable.tf
+++ varirable.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name"{
     description = "Name of Resource Group"
     type = string
-    default = "iac-devops-rg"
+    default = "iac-devops-rg-14"
 }
 
 variable "location" {
